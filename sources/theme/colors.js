40ddd4983b9195c7a8ef8e2383c137e97ead8d02
okshi <johnstarhafiz@gmail.com>
2022-08-24 17:52:29 +0600

    Loaded google fonts and set the color preset for this project

---
+++ sources/theme/colors.js
@@ -0,0 +1,13 @@
+export const colors = {
+  black: "#070742",
+  white: "#FFF",
+  darkGrey: "#38384F",
+  grey: "#838391",
+  lightGreen: "#419EBB",
+  orange: "#EDA249",
+  purple: "#6F2E2D",
+  darkOrange: "#D14C32",
+  red: "#D83A34",
+  green: "#1EC2A4",
+  blue: "#2D68F0",
+};
